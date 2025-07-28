626c514c86e5e805b2078a87b0e192d2a6348f81
Henil Mehta <henilm.kian@gmail.com>
2025-07-28 10:41:15 +0530

    ignore some files

--- AjaxCallExpress/index.js
+++ AjaxCallExpress/index.js
@@ -1,7 +1,32 @@
 const express = require('express');
 const app = express();
+app.set('view engine', 'ejs');
+app.set('views', './view-dir');
 app.use(express.static("./resources"));
 app.use(express.urlencoded({ extended: true }));
+
+
+app.route('/form').get((req, res) => {
+    res.render('form');
+}).post((req, res) => {
+    const { fname, lname } = req.body;
+    const errors = [];
+
+    if (!fname || fname.trim() === '') {
+        errors.push('First name is required.');
+    }
+
+    if (!lname || lname.trim() === '') {
+        errors.push('Last name is required.');
+    }
+
+    if (errors.length > 0) {
+        res.render('form', { errors });
+    } else {
+        res.render('form', { fname, lname });
+    }
+});
+
 
 app.get('/getData', (req, res) => {
     res.send('Welcome to the Ajax Call Express App!');
@@ -11,8 +36,6 @@ app.get('/getData', (req, res) => {
 app.post('/postData', (req, res) => {
     console.log(req.body.fname + " " + req.body.lname);
     res.send(req.body.fname + " " + req.body.lname);
-});
-
-app.listen(3000, () => {
+}).listen(3000, () => {
     console.log('Server is running on http://localhost:3000');
 });
